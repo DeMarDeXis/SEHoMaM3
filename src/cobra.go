c6e099cfa542460020e4f684c3d71b3a73f7421d
Aleksei Lysyakov <Alexrespmor@gmail.com>
2023-10-19 09:32:27 +0300

    Fixed #3 cobra.go

--- src/cobra.go
+++ src/cobra.go
@@ -1,21 +1,29 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/spf13/cobra"
+    "github.com/spf13/cobra"
+    "fmt"
+    "" // Замените на путь к вашему пакету savedata
 )
 
 func NewCobraCommand() *cobra.Command {
-	var rootCmd = &cobra.Command{Use: "SEHoMaM3"}
+    var rootCmd = &cobra.Command{Use: "SEHoMaM3"}
 
-	rootCmd.AddCommand(&cobra.Command{
-		Use:   "hello",
-		Short: "Prints 'Hello, SEHoMaM3'",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Hello, SEHoMaM3")
-		},
-	})
+    rootCmd.AddCommand(&cobra.Command{
+        Use:   "open",
+        Short: "Open a file",
+        Args:  cobra.ExactArgs(1),
+        Run: func(cmd *cobra.Command, args []string) {
+            filename := args[0]
+            sf := savedata.NewSaveFile(filename)
+            err := sf.Open()
+            if err != nil {
+                fmt.Printf("Error opening file: %v\n", err)
+            } else {
+                fmt.Printf("Opened file: %s\n", filename)
+            }
+        },
+    })
 
-	return rootCmd
+    return rootCmd
 }
